ed4a9bb657d01f01bc6ef1774e10d50e72dfdb98
Bharath Sivanesh.S <132342144+Bharathsivanesh@users.noreply.github.com>
2024-10-10 01:26:11 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ dotenv.config({ path: path.join(__dirname, "config", "config.env") });
 app.use(cors());
 connectDB();
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 const users = require("./routes/userRoutes");
 const blogs = require("./routes/userblogroutes");
@@ -18,6 +18,9 @@ const blogs = require("./routes/userblogroutes");
 app.use(express.json());
 app.use("/parental-blog/", users);
 app.use("/parental-blog/", blogs);
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
